dac1fc2dca30b15998c054a398a29af5ca9ec12a
Karl Shea <karl@weilstreet.com>
2022-08-16 11:50:13 -0500

    Subpath exports not working

--- shared-context/app2/src/Welcome.js
+++ shared-context/app2/src/Welcome.js
@@ -1,4 +1,4 @@
-import { NameContextProvider } from '@shared-context/shared-library';
+import { NameContextProvider } from '@shared-context/shared-library/test/NameContextProvider.js';
 import React from 'react';
 
 const Welcome = () => {
